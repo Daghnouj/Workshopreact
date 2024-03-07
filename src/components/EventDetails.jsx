0bef0bd8b28a8a3b915a94325772720efaf782f0
raouia <raouia.daghnouj@esprit.tn>
2024-03-07 19:57:39 +0100

    renduReact

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,4 +1,4 @@
-{/*import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function EventDetails(){
     const {title,id} = useParams();
@@ -8,25 +8,4 @@ function EventDetails(){
         <h1>EventDetails</h1>
     );
 }
-export default EventDetails;*/}
-
-import React from 'react';
-import '../App.css'; // Adjust the path based on the actual location of App.css
-
-const EventDetails = ({ event }) => {
-  return (
-    <div className="event-details">
-      <h1>{event.title} ({event.acronym})</h1>
-      <div className="event-description">
-        <strong>Description</strong>
-        <p>{event.description}</p>
-      </div>
-      <div className="event-price">
-        <strong>Price</strong>
-        <p>{event.price} TND</p>
-      </div>
-    </div>
-  );
-};
-
 export default EventDetails;
